cbfb74729768634d2fda9c42f2cde1da1e70c822
Nipun Nirmana <nipunfernando@hotmail.com>
2022-01-23 18:26:55 +0530

    Initial CRA changes including Installable PWA

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>START Page</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
